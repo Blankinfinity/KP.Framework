0550164fde4dd0e4f1b0415f0e6b9ef7b28325b2
Keith Pearce <keith@keith-pearce.co.uk>
2019-04-07 13:19:13 +0100

    Adding ldap, test project and framework. Added initial method

---
+++ KP.Framework/KP.Ldap/LdapManager.cs
@@ -0,0 +1,13 @@
+﻿using System.DirectoryServices;
+
+namespace KP.Ldap
+{
+    public class LdapManager
+    {
+        private string GetCurrentDomainPath()
+        {
+            var de = new DirectoryEntry("LDAP://RootDSE");
+            return de.Properties["defaultNamingContext"][0].ToString();
+        }
+    }
+}
